4fbc37fda6c8fe14d949dd9f36761efe5c473455
Amr109 <grand.109.master@gmail.com>
2022-10-04 00:21:34 +0300

    huge update v2

--- src/processes/postback.js
+++ src/processes/postback.js
@@ -1,39 +1,45 @@
 const request = require('request')
 const senderAction = require('../templates/senderAction')
 const sendMessage = require('../templates/sendMessage')
-const sendGenericTemplate = require('../templates/sendGenericTemplate')
 
-module.exports = function processMessage(event) {
-   if (!event.message.is_echo) {
-      const message = event.message
-      const senderID = event.sender.id
-      console.log('Received message from senderId: ' + senderID)
-      console.log('Message is: ' + JSON.stringify(message))
-      if (message.text) {
-         // now we will take the text recieved and send it to an food tracking API.
-         let text = message.text
-         var request = require('request')
+module.exports = function processPostback(event) {
+   const senderID = event.sender.id
+   const payload = event.postback.payload
 
-         let options = {
-            method: 'POST',
-            url: 'https://mefit-preprod.herokuapp.com/api/getnutritionvalue',
-            headers: {
-               'cache-control': 'no-cache',
-               'content-type': 'application/json',
+   if (payload === 'WELCOME') {
+      request(
+         {
+            url: 'https://graph.facebook.com/v15.0/' + senderID,
+            qs: {
+               access_token: process.env.PAGE_ACCESS_TOKEN,
+               fields: 'first_name',
             },
-            body: {
-               userID: process.env.USERID,
-               searchTerm: text,
-            },
-            json: true,
-         }
-
-         request(options, function (error, response, body) {
-            if (error) throw new Error(error)
+            method: 'GET',
+         },
+         function (error, response, body) {
+            let greeting = ''
+            if (error) {
+               console.error('Error getting user name: ' + error)
+            } else {
+               let bodyObject = JSON.parse(body)
+               console.log(bodyObject)
+               name = bodyObject.first_name
+               greeting = 'Hello ' + name + '. '
+            }
+            let message =
+               greeting + 'Welcome to Healthbot. Hope you are doing good today'
+            let message2 = 'I am your nutrition tracker :-)'
+            let message3 =
+               'please type in what you ate like: I ate chicken birayani and 2 chapatis with dal.'
             senderAction(senderID)
-            // after the response is recieved we will send the details in a Generic template
-            sendGenericTemplate(senderID, body)
-         })
-      }
+            sendMessage(senderID, { text: message }).then(() => {
+               sendMessage(senderID, { text: message2 }).then(() => {
+                  sendMessage(senderID, { text: message3 }).then(() => {
+                     sendMessage(senderID, { text: '🎈' })
+                  })
+               })
+            })
+         }
+      )
    }
 }
