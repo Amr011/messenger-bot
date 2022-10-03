f10d5e7ba782bdec30e96259d4ed531cba27b9f8
Amr109 <grand.109.master@gmail.com>
2022-10-03 20:34:03 +0300

    fixed routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,41 @@ const express = require('express')
 
 const router = express.Router()
 
+const processPostback = require('../processes/postback')
+const processMessage = require('../processes/messages')
+
 router.route('/').get((req, res) => {
-  return res.status(200).json('It Works')
+   return res.status(200).json('It Works')
+})
+
+router.route('/webhook').get(function (req, res) {
+   if (req.query['hub.verify_token'] === process.env.VERIFY_TOKEN) {
+      console.log('webhook verified')
+      res.status(200).send(req.query['hub.challenge'])
+   } else {
+      console.error('verification failed. Token mismatch.')
+      res.sendStatus(403)
+   }
+})
+
+router.route('/webhook').get(function (req, res) {
+   //checking for page subscription.
+   if (req.body.object === 'page') {
+      /* Iterate over each entry, there can be multiple entries 
+       if callbacks are batched. */
+      req.body.entry.forEach(function (entry) {
+         // Iterate over each messaging event
+         entry.messaging.forEach(function (event) {
+            console.log(event)
+            if (event.postback) {
+               processPostback(event)
+            } else if (event.message) {
+               processMessage(event)
+            }
+         })
+      })
+      res.sendStatus(200)
+   }
 })
 
 module.exports = router
